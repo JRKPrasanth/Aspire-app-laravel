adefd759b763d1ea602e263592b852301998878a
JRKPrasanth <prasanth_k@jrkresearch.com>
2026-04-13 15:42:45 +0530

    test 5

--- app/Http/Controllers/Api/UserAnalyticsController.php
+++ app/Http/Controllers/Api/UserAnalyticsController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Validator;
 
 class UserAnalyticsController extends Controller
 {
-    /** auto deploy checking 4
+    /** auto deploy checking 5
      * Log analytics events in batch
      * 
      * POST /api/analytics/batch
